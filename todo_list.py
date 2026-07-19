9032ad81a08e143fdfda0248ef2e7be7616d0dbb
JudithWLee <judithwlee.ee04@g2.nctu.edu.tw>
2026-07-19 22:24:19 +0800

    instead of i for insert, use O for insert above and o for insert below
    Signed-off-by: JudithWLee <judithwlee.ee04@g2.nctu.edu.tw>

--- todo_list.py
+++ todo_list.py
@@ -5,25 +5,36 @@ class ToDoList(ListView):
     BINDINGS = [
         ("k", "cursor_up", "Move cursor up"),
         ("j", "cursor_down", "Move cursor down"),
-        ("d", "delete_to_do", "Delete highlighted to do"),
-        ("i", "insert_to_do", "Insert a new to do"),
+        ("d", "delete_todo", "Delete highlighted to do"),
+        ("O", "insert_todo_above", "Insert a new to do above"),
+        ("o", "insert_todo_below", "Insert a new to do below"),
     ]
 
-    def action_delete_to_do(self) -> None:
+    def action_delete_todo(self) -> None:
         """Delete highlighted to do."""
         if self.index is not None:
             self.pop(self.index)
 
-    def action_insert_to_do(self) -> None:
+    def _insert_todo(self, new_todo_index: int, old_todo_index: int) -> None:
         """Add new to do."""
-        current_index = self.index
-        new_to_do = ToDoItem()
+        new_todo = ToDoItem()
 
-        self.insert(current_index, [new_to_do])
+        self.insert(new_todo_index, [new_todo])
 
         # Update the highlight to the new object
-        new_to_do.highlighted = True
-        self.children[current_index+1].highlighted = False
+        new_todo.highlighted = True
+        self.children[old_todo_index].highlighted = False
+        self.index = new_todo_index
+
+    def action_insert_todo_above(self) -> None:
+        new_todo_index = self.index
+        old_todo_index = self.index + 1
+        self._insert_todo(new_todo_index, old_todo_index)
+
+    def action_insert_todo_below(self) -> None:
+        new_todo_index = self.index + 1
+        old_todo_index = self.index
+        self._insert_todo(new_todo_index, old_todo_index)
 
     def on_list_view_selected(self, event: ListView.Selected) -> None:
         """If user clicks on item."""
