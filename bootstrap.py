36bdc185e1f0ccf1812aec9d2894f5c503be0077
JudithWLee <judithwlee.ee04@g2.nctu.edu.tw>
2026-07-19 22:04:44 +0800

    refactor functions to suitable classes
    Signed-off-by: JudithWLee <judithwlee.ee04@g2.nctu.edu.tw>

--- bootstrap.py
+++ bootstrap.py
@@ -3,10 +3,35 @@ from textual.app import App, ComposeResult
 from textual.widgets import Footer, Header, ListItem, ListView, Label, Input
 from textual.css.query import NoMatches
 
-class ToDoItem(Label):
-    def __init__(self, content: str):
-        super().__init__(content)
+class ToDoItem(ListItem):
+    def __init__(self, content: str = None):
+        super().__init__()
         self.content = content
+
+    def compose(self) -> ComposeResult:
+        if self.content is not None:
+            yield Label(self.content)
+        else:
+            yield Input(placeholder = "New to do item")
+
+    def select(self) -> None:
+        """If user clicks on item."""
+        child = self.query_one("*")
+
+        if isinstance(child, Label):
+            if self.has_class("completed"):
+                child.update(child.content.removeprefix("\[DONE] "))
+            else:
+                child.update(f"\[DONE] {child.content}")
+
+            self.toggle_class("completed")
+        elif isinstance(child, Input):
+            child.focus()
+        else:
+            self.log("Child type not supported yet, plz implement")
+
+    def submit_content(self) -> None:
+        pass
 
 class ToDoList(ListView):
     BINDINGS = [
@@ -24,14 +49,31 @@ class ToDoList(ListView):
     def action_insert_to_do(self) -> None:
         """Add new to do."""
         current_index = self.index
-        new_input = Input(placeholder = "New to do item")
-        new_to_do = ListItem(new_input)
+        new_to_do = ToDoItem()
 
         self.insert(current_index, [new_to_do])
 
         # Update the highlight to the new object
         new_to_do.highlighted = True
         self.children[current_index+1].highlighted = False
+
+    def on_list_view_selected(self, event: ListView.Selected) -> None:
+        """If user clicks on item."""
+        event.item.select()
+
+    def on_input_submitted(self, event: Input.Submitted) -> None:
+        # Take the string submitted
+        input_value = event.value
+
+        current_index = self.index
+        # Remove the item being selected
+        self.pop(current_index)
+        # Insert new to do
+        new_to_do = ToDoItem(input_value)
+        self.insert(current_index, [new_to_do])
+        new_to_do.highlighted == True
+
+        self.focus()
 
 class BootstrapApp(App):
     CSS_PATH = "to_do_list.tcss"
@@ -44,41 +86,12 @@ class BootstrapApp(App):
     def compose(self) -> ComposeResult:
         yield Header()
         yield ToDoList(
-            ListItem(ToDoItem("Implement inputting stuff into the new input")),
-            ListItem(ToDoItem("Implement turning the input into new to do")),
-            ListItem(ToDoItem("Implement to do content editing")),
-            ListItem(ToDoItem("Refactor bindings into ToDoList")),
+            ToDoItem("Implement inputting stuff into the new input"),
+            ToDoItem("Implement turning the input into new to do"),
+            ToDoItem("Implement to do content editing"),
+            ToDoItem("Refactor bindings into ToDoList"),
         )
         yield Footer()
-
-    def on_list_view_selected(self, event: ToDoList.Selected) -> None:
-        """If user clicks on item."""
-        try:
-            todo = event.item.query_one(ToDoItem)
-            if event.item.has_class("completed"):
-                self.log(repr(todo.content))
-                todo.update(todo.content.removeprefix("\[DONE] "))
-            else:
-                todo.update(f"\[DONE] {todo.content}")
-            event.item.toggle_class("completed")
-        except NoMatches:
-            input_block = event.item.query_one(Input)
-            input_block.focus()
-
-    def on_input_submitted(self, event: Input.Submitted) -> None:
-        # Take the string submitted
-        input_value = event.value
-
-        to_do_list = self.query_one(ToDoList)
-        current_index = to_do_list.index
-        # Remove the item being selected
-        to_do_list.pop(current_index)
-        # Insert new to do
-        new_to_do = ListItem(ToDoItem(input_value))
-        to_do_list.insert(current_index, [new_to_do])
-        new_to_do.highlighted == True
-
-        to_do_list.focus()
 
 if __name__ == "__main__":
     app = BootstrapApp()
